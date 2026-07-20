96466d7129a56a623dee13dce932f132255670f8
arhab <arhabfaiz-arch-epep.com>
2026-07-20 09:56:37 +0700

    materi-dart-bab3

---
+++ bab3/typedata.dart
@@ -0,0 +1,11 @@
+void main () {
+    String nama = 'faiz';
+    int umur = 19;
+    double tinggi =189.6;
+    bool sudahlulus = false;
+
+    print ('nama: $nama');
+    print ('umur: $umur tahun');
+    print ('tinggi: $tinggi cm');
+    print ('sudah lulus: $sudahlulus');
+}
