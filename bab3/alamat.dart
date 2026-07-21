848c8f913271733d2d867440c2292229486aced0
arhab <arhabfaiz-arch-epep.com>
2026-07-21 08:29:12 +0700

    update

---
+++ bab3/alamat.dart
@@ -0,0 +1,12 @@
+void main () {
+    var alamat  = 'Jl. perkutut no.13';
+    alamat = 'Jl. perkutut no.14';
+
+    final String nisn = '1234567';
+
+    const double pi = 3.12;
+
+    print ('alamat: $alamat');
+    print ('nisn: $nisn');
+    print ('pi: $pi');
+}
